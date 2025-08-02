3c3d10e36d8a46e5bffa967fdb1be276b8e2a49b
riya1571 <smriya494@gmail.com>
2025-08-02 15:08:34 +0600

    2ndvdoquestion2 solved

---
+++ 2ndvdoquestion2.js
@@ -0,0 +1,22 @@
+
+const weight = 44;
+const height = 1.63;
+
+BMI  = weight / (height**2);
+
+
+if(BMI < 18.5){
+    console.log("You are underweight");
+}
+
+else if(BMI>=18.5 && BMI <=24.9){
+    console.log("You are normal.");
+}
+
+else if( BMI>=25 && BMI <= 29.9){
+    console.log("You are overweight.");
+}
+
+else{
+    console.log("You are obese.");
+}
