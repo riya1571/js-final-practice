3f2125d02c63c11b9f4a1a5fc2247b125e54cf9a
riya1571 <smriya494@gmail.com>
2025-08-02 21:37:20 +0600

    5th problem Solved

---
+++ 2ndvdoquestion4.js
@@ -0,0 +1,20 @@
+// const num1 = 10 ;
+// const num2= 5 ;
+
+// let result;
+
+// if(num1>num2){
+//     console.log(result=num1*2);
+// }
+// else{
+//     console.log(result= num1+num2);
+// }
+
+
+// ternary operator
+let num1 = 10;
+let num2 = 5;
+
+let result = (num1 > num2) ? num1 * 2 : num1 +  num2;
+
+console.log(result);
