bb79e6ebd052c3fb68c8ce9fd13c2c61a314b6c8
riya1571 <smriya494@gmail.com>
2025-08-02 14:24:05 +0600

    question2 solved

---
+++ question2.js
@@ -0,0 +1,7 @@
+const mathematics = 75.25;
+const biology = 65;
+const chemistry = 80;
+const physics = 35.45;
+const bangla = 99.50;
+
+console.log(((mathematics+biology+chemistry+physics+bangla)/5).toFixed(2));
