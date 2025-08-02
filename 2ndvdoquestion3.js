eb6eecc69a53b8d142a969ce289cfa78248619e5
riya1571 <smriya494@gmail.com>
2025-08-02 20:57:35 +0600

    3rd question solved

---
+++ 2ndvdoquestion3.js
@@ -0,0 +1,22 @@
+const mark = 88;
+
+if(mark>=90){
+    console.log("A");
+
+}
+
+else if (mark>=80){
+    console.log("B");
+}
+
+else if (mark>=70){
+    console.log("C");
+}
+
+else if (mark>=60){
+    console.log("D");
+}
+
+else if (mark<=59){
+    console.log("F");
+}
