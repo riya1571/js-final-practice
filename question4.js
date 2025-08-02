f00235f5b7f6a6bce187b61eb8bfff4bdc3b3c26
riya1571 <smriya494@gmail.com>
2025-08-02 14:38:41 +0600

    question4 solved

---
+++ question4.js
@@ -0,0 +1,5 @@
+var a = isNaN(2-10); //-8 negative number holew , number ee.. tai false ashbe.. mane -8 not a number _ ei kotha ta vul..
+console.log(a);
+
+var b = isNaN('11');
+console.log(b); //cz ekhane string take number a cnvrt kore niche..
