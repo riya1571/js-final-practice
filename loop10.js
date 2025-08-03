91232841870518b2f0db0d866f5603e002c47bab
riya1571 <smriya494@gmail.com>
2025-08-03 18:44:21 +0600

    break using while loop

---
+++ loop10.js
@@ -0,0 +1,11 @@
+let i=1;
+let sum =0;
+
+while(i<=200){
+    sum=sum+i;
+    if(sum>=200){
+        break;
+    }
+    i++;
+}
+console.log(sum);
---
+++ loop9.js
@@ -0,0 +1,6 @@
+for(i=1; i<=200;i++){
+    if(i===101){
+        break
+    }
+    console.log(i);
+}
